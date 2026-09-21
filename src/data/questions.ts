7f5e997a1cf496c026c08ddfa9005e7dfe9e692d
woodyfaithsong3-netizen <woodyfaithsong3@gmail.com>
2026-09-21 21:56:26 +0200

    V107 characters: integrate secondary character wave

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -8,6 +8,7 @@ import { jwV61Quote, jwV61Forbidden } from './jw_enrichment_v61';
 import { jwV104Quiz, jwV104TrueFalse, jwV104Mystery } from './jw_enrichment_v104';
 import { jwV105Quiz, jwV105TrueFalse, jwV105Mystery } from './jw_enrichment_v105';
 import { jwV106CharacterQuiz, jwV106Mystery } from './jw_enrichment_v106_characters';
+import { jwV107CharacterQuiz, jwV107Mystery } from './jw_enrichment_v107_characters';
 import { Challenge, ChronologyQuestion, Difficulty, IntruderQuestion, MysteryQuestion, QuizQuestion, QuoteQuestion, TimesUpQuestion, TrueFalseQuestion } from '@/types';
 import { categoryQuizExpansion, categoryTrueFalseExpansion, categoryMysteryExpansion, categoryTimesUpExpansion, categoryQuoteExpansion, categoryChronologyExpansion, categoryIntruderExpansion, categoryChallengeExpansion } from './jwCategories';
 
@@ -1623,6 +1624,8 @@ quizQuestions.push(...jwV104Quiz);
 quizQuestions.push(...jwV105Quiz);
 quizQuestions.push(...jwV106CharacterQuiz);
 mysteryQuestions.push(...jwV106Mystery);
+quizQuestions.push(...jwV107CharacterQuiz);
+mysteryQuestions.push(...jwV107Mystery);
 trueFalseQuestions.push(...jwV105TrueFalse);
 mysteryQuestions.push(...jwV105Mystery);
 trueFalseQuestions.push(...jwV104TrueFalse);
