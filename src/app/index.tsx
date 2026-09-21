13ed3e3ceb250f250db594563db893e281b7bf11
woodyfaithsong3-netizen <woodyfaithsong3@gmail.com>
2026-09-21 23:48:18 +0200

    Android: open directly on the game entry screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Platform } from 'react-native';
+import { Redirect } from 'expo-router';
 import LandingScreen from '@/components/LandingScreen';
 
 export default function HomeScreen() {
+  if (Platform.OS !== 'web') {
+    return <Redirect href="/play" />;
+  }
+
   return <LandingScreen />;
 }
